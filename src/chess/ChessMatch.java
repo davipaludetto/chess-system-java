fecb446133b87d969f504291474d631962fbf584
Davi Paludetto <daviortiz.p@gmail.com>
2025-08-26 16:35:02 -0300

    Switching player each turn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,11 +8,23 @@ import chess.pieces.Rook;
 
 public class ChessMatch { // coração sistema com as regras do jogo
 	 
+	private int turn;
+	private Color currentPlayer;
 	private Board board;
 	
 	public ChessMatch() {
 		board = new Board (8, 8);
+		turn = 1;
+		currentPlayer = Color.WHITE;
 		initialSetup();
+	}
+	
+	public int getTurn() {
+		return turn;
+	}
+	
+	public Color getCurrentPlayer() {
+		return currentPlayer;
 	}
 
 	public ChessPiece[][] getPieces(){
@@ -37,6 +49,7 @@ public class ChessMatch { // coração sistema com as regras do jogo
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
 	
@@ -51,6 +64,9 @@ public class ChessMatch { // coração sistema com as regras do jogo
 		if (!board.thereIsAPiece(position)) {
 			throw new ChessException("There is no piece on source positon");
 		}
+		if (currentPlayer != ((ChessPiece)board.piece(position)).getColor()) {
+			throw new ChessException("The chosen piece is not yours");
+		}
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("There is no possible moves for the chosen piece");
 		}
@@ -61,6 +77,14 @@ public class ChessMatch { // coração sistema com as regras do jogo
 			throw new ChessException("The chosen piece can't move to target position");
 		}
 	}
+	
+	 private void nextTurn() {
+		 turn++;
+		 currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE; // expressão condicional ternária
+	 }
+	
+	
+	
 	
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
