05c5fe65edbcd3dae7d72c6bc8395caa9b254267
Davi Paludetto <daviortiz.p@gmail.com>
2025-08-27 17:38:31 -0300

    Check logic

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -2,6 +2,7 @@ package chess;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import boardgame.Board;
 import boardgame.Piece;
@@ -14,6 +15,7 @@ public class ChessMatch { // coração do sistema com as regras do jogo
 	private int turn;
 	private Color currentPlayer;
 	private Board board;
+	private boolean check;
 	
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -31,6 +33,10 @@ public class ChessMatch { // coração do sistema com as regras do jogo
 	
 	public Color getCurrentPlayer() {
 		return currentPlayer;
+	}
+	
+	public boolean getCheck() {
+		return check;
 	}
 
 	public ChessPiece[][] getPieces(){
@@ -55,6 +61,14 @@ public class ChessMatch { // coração do sistema com as regras do jogo
 		validateSourcePosition(source);
 		validateTargetPosition(source, target);
 		Piece capturedPiece = makeMove(source, target);
+		
+		if (testCheck(currentPlayer)) {
+			undoMove(source, target, capturedPiece);
+			throw new ChessException("You can't put yourself in check");
+		}
+		
+		check = (testCheck(opponent(currentPlayer))) ?  true : false;
+		
 		nextTurn();
 		return (ChessPiece)capturedPiece;
 	}
@@ -70,6 +84,17 @@ public class ChessMatch { // coração do sistema com as regras do jogo
 		}
 		
 		return capturedPiece;
+	}
+	
+	private void undoMove(Position source, Position target, Piece capturedPiece) {
+		Piece p = board.removePiece(target);
+		board.placePiece(p, source);
+		
+		if (capturedPiece != null) {
+			board.placePiece(capturedPiece, target);
+			capturedPieces.remove(capturedPiece);
+			piecesOnTheBoard.add(capturedPiece);
+		}
 	}
 	
 	private void validateSourcePosition(Position position) { // duas verificações da peça de origem
@@ -94,7 +119,33 @@ public class ChessMatch { // coração do sistema com as regras do jogo
 		 turn++;
 		 currentPlayer = (currentPlayer == Color.WHITE) ? Color.BLACK : Color.WHITE; // expressão condicional ternária
 	 }
+	 
+	 private Color opponent(Color color) {
+		 return (color == Color.WHITE) ? Color.BLACK : Color.WHITE;
+	 }
+	 
+	 private ChessPiece king(Color color) { // filtrando listas com expressões lambda
+		 List<Piece> list = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == color).collect(Collectors.toList());
+		 for (Piece p : list) {
+			 if (p instanceof King) {
+				 return (ChessPiece)p;
+			 }
+		 }
+		 throw new IllegalStateException("There is no " + color + " king on the board");
+	 }
 	
+	 private boolean testCheck(Color color) {
+		 Position kingPosition = king(color).getChessPosition().toPosition();
+		 List<Piece> opponentPieces = piecesOnTheBoard.stream().filter(x -> ((ChessPiece)x).getColor() == opponent(color)).collect(Collectors.toList());
+		 for (Piece p : opponentPieces) {
+			 boolean[][] mat = p.possibleMoves();
+			 if (mat[kingPosition.getRow()][kingPosition.getColumn()]) {
+				 return true;
+			 }
+		 }
+		 return false;
+	 }
+	 
 	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.placePiece(piece, new ChessPosition(column, row).toPosition());
 		piecesOnTheBoard.add(piece);
