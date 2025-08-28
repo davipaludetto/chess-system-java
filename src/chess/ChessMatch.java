41b37a33e2cd9d684d45e86421aa078092648e85
Davi Paludetto <daviortiz.p@gmail.com>
2025-08-28 14:55:12 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -83,7 +83,8 @@ public class ChessMatch { // coração do sistema com as regras do jogo
 	}
 	
 	private Piece makeMove(Position source, Position target) {
-		Piece p = board.removePiece(source);
+		ChessPiece p = (ChessPiece)board.removePiece(source);
+		p.increaseMoveCount();
 		Piece capturedPiece = board.removePiece(target);
 		board.placePiece(p, target);
 		
@@ -96,7 +97,8 @@ public class ChessMatch { // coração do sistema com as regras do jogo
 	}
 	
 	private void undoMove(Position source, Position target, Piece capturedPiece) {
-		Piece p = board.removePiece(target);
+		ChessPiece p = (ChessPiece)board.removePiece(target);
+		p.decreaseMoveCount();
 		board.placePiece(p, source);
 		
 		if (capturedPiece != null) {
